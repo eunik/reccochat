40eaed609c0ee2935918e4b3ad14024f0f68d3c6
eunik <k.eun121@gmail.com>
2018-11-16 01:41:20 -0500

    initial commit

---
+++ controllers/index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+
+
+router.use('/alt', require('./alt'));
+router.use('/', require('./home'));
+router.use('/auth', require('./auth'));
+module.exports = router;
