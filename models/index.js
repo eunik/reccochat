82e83aa95f0873153f557063b064dcd5fcb6a615
eunik <k.eun121@gmail.com>
2018-12-12 20:49:57 -0500

    modifying for horoku deployment

--- models/index.js
+++ models/index.js
@@ -9,13 +9,7 @@ var config    = require(__dirname + '/../config/config.json')[env];
 var db        = {};
 
 if (config.use_env_variable) {
-  var sequelize = new Sequelize(process.env.config.use_env_variable, {
-      dialect:  'postgres',
-      protocol: 'postgres',
-      port:     match[4],
-      host:     match[3],
-      logging:  true //false
-    }));
+  var sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
   var sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
