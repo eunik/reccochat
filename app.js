d956d835a044d0fd8d4e877c9f0f77e45a9a52a0
eunik <k.eun121@gmail.com>
2018-12-12 21:04:28 -0500

    modifying for horoku deployment

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const express = require('express');
 const expressSession = require('express-session');
 const passport = require('./middlewares/authentication');
 
-const PORT = process.env.PORT || 3000;
+var port = process.env.PORT || 3000;
+
+app.use(express.static(__dirname + '/public'));
 
 const app = express();
 
@@ -10,19 +12,6 @@ const app = express();
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.bodyParser());
-app.use(express.methodOverride());
-app.use(app.router);
-app.use(express.static(path.join(__dirname, 'public')));
-
-// development only
-if ('development' === app.get('env')) {
-  app.use(express.errorHandler());
-}
-
 
 // Enable sessions & passport
 app.use(expressSession(({ secret: 'keyboard cat', resave: false, saveUninitialized: true })));
